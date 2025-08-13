c59330c07a4e2cd28cb3ca41b967e93850b94b03
Eduarda Rafaella <erafaella88@gmail.com>
2025-08-13 05:17:20 -0300

    {1000000000{EncontrandooMaiorNúmero}}

---
+++ EncontrandooMaiorNúmero.py
@@ -0,0 +1,9 @@
+def encontrar_maior_numero(lista):
+
+    return max(lista) 
+
+lista = [987654321,2,7654321,56,1234567, 1, 88888,3,42,999999,5,1000000000,13,101010,7,444, 9, 2, 13, 9]
+
+maior = encontrar_maior_numero(lista)
+
+print("O maior número desta lista é: ", maior)
