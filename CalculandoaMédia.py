89e840ef7a613c88ac97f5dcb27622216f8a14b7
Eduarda Rafaella <erafaella88@gmail.com>
2025-08-13 07:11:44 -0300

    {99886685.6{CalculandoaMedia}}

---
+++ CalculandoaMédia.py
@@ -0,0 +1,6 @@
+def calcula_media(lista):
+    return sum(lista) / len(lista)
+
+lista = [987654321,2,7654321,56,1234567, 1, 88888,3,42,999999,5,1000000000,13,101010,7,444, 9, 2, 13, 9]
+
+print("A média dos elementos desta lista é: ", calcula_media(lista)) 
